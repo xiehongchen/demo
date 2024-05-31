2b96fb0db09ee8139d7280ad01b47d882e8553c7
xiehongchen <1754581057@qq.com>
2024-05-31 18:24:06 +0800

    base地址修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const outputPath = process.env.NODE_ENV === 'production' ? '/vue-demo' : ''
+const outputPath = process.env.NODE_ENV === 'production' ? '/demo' : ''
 // https://vitejs.dev/config/
 export default defineConfig({
   base: outputPath,
